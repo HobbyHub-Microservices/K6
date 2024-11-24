b3350ad352da652d7330e1e05c0a63540fe290c7
JANINE <478518@student.fontys.nl>
2024-11-24 15:32:21 +0100

    First commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+﻿FROM loadimpact/k6:latest
+
+# Copy your k6 script into the container
+COPY loadtest.js /scripts/loadtest.js
+
+# Set the default command to run the k6 script
+CMD ["run", "/scripts/loadtest.js"]
---
+++ loadtest.js
@@ -0,0 +1,19 @@
+﻿import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    // vum: 300,
+    // duration: '2m'
+    
+    stages: [
+        { target: 200, duration: '30s' },
+        { target: 0, duration: '30s' },
+    ],
+};
+
+export default function () {
+    const result = http.get('http://hobbyhub.com/api/h/users');
+    check(result, {
+        'http response status code is 200': result.status === 200,
+    });
+}
